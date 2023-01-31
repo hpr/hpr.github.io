abc043d5ef21f549f01fef7c8b15404c3cf5039f
Harry <habs@sdf.org>
2023-01-31 06:57:55 -0500

    use materialui

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { areaChampionshipGroups, fieldSizes, nationalChampionshipGroups, nationalChampionshipOverrideIds } from './constants';
 import { CalendarEvent, EventName, GetCalendarCompetitionResults, Ranking, SexName } from './types';
@@ -8,6 +9,7 @@ const App = () => {
   const [endDate] = useState('2022-06-26');
   const [evt, setEvt] = useState<EventName>('1500m');
   const [sex, setSex] = useState<SexName>('men');
+  const [time, setTime] = useState<string>('3:45.00');
   const [rankings, setRankings] = useState<Ranking[]>([]);
   const [results, setResults] = useState<{ [meetId: string]: GetCalendarCompetitionResults }>({});
   const [competitions, setCompetitions] = useState<CalendarEvent[]>([]);
@@ -33,22 +35,33 @@ const App = () => {
   useEffect(() => {
     if (rankings.length) setTargetScore(+rankings[fieldSizes[evt] - 1].score);
   }, [rankings]);
-  let time = '3:44.00';
+
   const meetPoints = Object.values(results)
     .filter((x) => x)
     .flatMap((meet) => getScores(meet, evt, sex, time))
     .sort((a, b) => b.score - a.score)
-    .filter(({ meetId }, i, arr) => {
-      const meet = competitions.find(meet => meet.id === +meetId);
+    .filter(({ meetId }) => {
+      const meet = competitions.find((meet) => meet.id === +meetId);
       if ([...areaChampionshipGroups, ...nationalChampionshipGroups].includes(meet?.competitionGroup!)) return false;
       if (nationalChampionshipOverrideIds.includes(+meetId)) return false;
       return true;
     })
     .slice(0, 5);
+
   return (
-    <div>
+    <Box sx={{ minWidth: 120, textAlign: 'center' }}>
+      <FormControl sx={{ marginTop: 2 }}>
+        <InputLabel id="event-label">Event</InputLabel>
+        <Select labelId="event-label" label="Event" value={evt} onChange={(e) => setEvt(e.target.value as EventName)}>
+          {['800m', '1500m', '5000m'].map((evt) => (
+            <MenuItem key={evt} value={evt}>
+              {evt}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
       <div>Target Score: {targetScore}</div>
-      <div>1500m PB: {time}</div>
+      <div>{evt} PB: {time}</div>
       <div>Strategic Top 5:</div>
       <ol>
         {meetPoints.map(({ meet, startDate, place, points, score, placeBonus, meetId }, i) => {
@@ -60,7 +73,7 @@ const App = () => {
         })}
       </ol>
       <div>Average score: {meetPoints.reduce((acc, x) => acc + x.score, 0) / 5}</div>
-    </div>
+    </Box>
   );
 };
 
